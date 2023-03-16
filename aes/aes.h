94d061f5639ba517c33b915b58cf60a28dadc6ef
martian <26498920+c0m94d3@users.noreply.github.com>
2023-03-16 22:23:12 +0530

    some documentation a day keeps the questions away?

--- aes/aes.h
+++ aes/aes.h
@@ -1,11 +1,24 @@
 //
 // Created by martian on 3/13/2023.
 //
-
 #ifndef AES_AES_H
 #define AES_AES_H
 
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Functions from aes.c
+ */
+
 uint8_t gfadd(uint8_t x, uint8_t y);
 uint8_t gfmul(uint8_t x, uint8_t y);
+
+/*
+ * Other
+ */
+
+int gentable();
 
 #endif //AES_AES_H
