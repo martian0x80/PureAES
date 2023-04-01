3255ccb098fd157728fb23815a1a477d47a46afa
martian <26498920+c0m94d3@users.noreply.github.com>
2023-04-01 18:43:51 +0530

    InverseCipher, EqInverseCipher and its components. Quick commit before test and mess something up.

--- aes.c
+++ aes.c
@@ -11,7 +11,7 @@
 #define getStateArr(arr, N_b, x, y) arr[x * N_b + y]
 
 // Helper macro to get the substitution byte for x. For debugging.
-#define getSBOX(x) sbox[((x >> 4) & 0xf) * 0x10 + (x & 0xf)]
+#define getSBOX(x) Sbox[((x >> 4) & 0xf) * 0x10 + (x & 0xf)]
 
 #define LITTLE_ENDIAN 1
 #define BIG_ENDIAN 0
@@ -47,7 +47,7 @@ const uint16_t PP = 0x11b;
 static const uint32_t Rcon[10] = {0x01000000, 0x02000000, 0x04000000, 0x08000000, 0x10000000, 0x20000000, 0x40000000,
 								  0x80000000, 0x1b000000, 0x36000000};
 
-static const uint8_t sbox[256] =
+static const uint8_t Sbox[256] =
 		{0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76, 0xca, 0x82,
 		 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0, 0xb7, 0xfd, 0x93, 0x26,
 		 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15, 0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96,
@@ -64,6 +64,23 @@ static const uint8_t sbox[256] =
 		 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf, 0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f,
 		 0xb0, 0x54, 0xbb, 0x16};
 
+static const uint8_t invSbox[256] =
+		{0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
+		 0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
+		 0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
+		 0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
+		 0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+		 0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
+		 0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
+		 0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
+		 0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
+		 0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
+		 0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
+		 0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
+		 0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
+		 0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
+		 0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
+		 0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D};
 /*
  * The addition of two elements in a finite field is achieved by adding the coefficients for the
  * corresponding powers in the polynomials for the two elements. The addition is performed with
@@ -157,7 +174,7 @@ void gfmul_words(const uint8_t *a, uint8_t *b, uint8_t *c) {
  * 
  */
 
-static uint32_t RotWord(uint32_t word) { 		// Cyclic left shift
+static uint32_t RotWord(uint32_t word) { 		// Cyclic left shift, this can be easily extended for n shifts
 	return ((word << 0x8) & 0xffffffff) | ((word >> 0x18) & 0x000000ff);
 }
 
@@ -176,8 +193,8 @@ static uint32_t RotWord(uint32_t word) { 		// Cyclic left shift
 static void SubBytes(uint8_t stateArray[]) {
 	for (int i = 0; i < N_b * 4; i++) {
 		uint8_t toSub = stateArray[i];
-		stateArray[i] = sbox[((toSub >> 4) & 0xf) * 0x10 +
-							 (toSub & 0xf)]; // GET_ELEM(sbox, (toSub>>4)&0xf, toSub&0xf, 0x10)
+		stateArray[i] = Sbox[((toSub >> 4) & 0xf) * 0x10 +
+							 (toSub & 0xf)]; // GET_ELEM(Sbox, (toSub>>4)&0xf, toSub&0xf, 0x10)
 	}
 }
 
@@ -188,8 +205,8 @@ static uint32_t SubWords(uint32_t word) {
 		uint8_t toSub = (word >> (i * 8)) & 0xff;
 		// I had to smash my brain into gdb to figure out that + has higher precedence than & operator, also the endianness, fuck me
 		// The left and right shift operations are independent of the byte order of the system, but *(uint32_t*) is not.
-		subWord[ENDIANNESS ? i : 3 - i] = sbox[((toSub >> 4) & 0xf) * 0x10 +
-											   (toSub & 0xf)]; // GET_ELEM(sbox, (toSub>>4)&0xf, toSub&0xf, 0x10)
+		subWord[ENDIANNESS ? i : 3 - i] = Sbox[((toSub >> 4) & 0xf) * 0x10 +
+											   (toSub & 0xf)]; // GET_ELEM(Sbox, (toSub>>4)&0xf, toSub&0xf, 0x10)
 	}
 	return *(uint32_t *) subWord;
 }
@@ -297,6 +314,7 @@ void KeyExpansion(const uint8_t key[], uint32_t expandedKey[]) {
 	}
 }
 
+
 uint8_t* CipherEncrypt(uint8_t stateArray[], const uint32_t roundKeys[]) {
 	uint32_t w[N_b];
 	for (int i = 0; i < N_b; i++) {
@@ -385,8 +403,8 @@ static void InvShiftRows(uint8_t stateArray[]) {
 static void InvSubBytes(uint8_t stateArray[]) {
 	for (int i = 0; i < N_b * 4; i++) {
 		uint8_t toSub = stateArray[i];
-		stateArray[i] = inv_sbox[((toSub >> 4) & 0xf) * 0x10 +
-							 (toSub & 0xf)]; // GET_ELEM(sbox, (toSub>>4)&0xf, toSub&0xf, 0x10)
+		stateArray[i] = invSbox[((toSub >> 4) & 0xf) * 0x10 +
+								(toSub & 0xf)]; // GET_ELEM(Sbox, (toSub>>4)&0xf, toSub&0xf, 0x10)
 	}
 }
 
@@ -405,6 +423,123 @@ static void InvMixColumns(uint8_t stateArray[]) {
 			getStateArr(stateArray, N_b, j, i) = polyProd[j];
 		}
 	}
+}
+
+/*
+ * Modified InvMixColumns() transformation that acts on round keys taken as words.
+ */
+
+static void EqInvMixColumns(uint32_t roundKeys[]) {
+	static const uint8_t a_x_inv[] = {0x0b, 0x0d, 0x09, 0x0e};
+	// We are essentially doing three operations consecutively, first being extracting the column array or word, and then applying the transformation, and in the end storing it back in place
+	for (int i = 0; i < N_b; i++) {                    // Iterating through columns
+		uint8_t polyProd[4];
+		uint8_t roundKeyArray[4] = {(roundKeys[i] >> 24) & 0xff, (roundKeys[i] >> 16) & 0xff,
+									(roundKeys[i] >> 8) & 0xff, (roundKeys[i]) & 0xff};
+		// Could've just defined gfmul_words() to take uint32_t instead of uint8_t, but a lot of the code above depends on it
+		gfmul_words(a_x_inv, roundKeyArray, polyProd);                // Product of the polynomial mod x^4 + 1
+		// Stuffing it in back in the roundKeys array as a uint32_t (word)
+		roundKeys[i] = ((polyProd[0] << 24) & 0xffffffff) | ((polyProd[1] << 16) & 0xffffffff) | ((polyProd[2] << 8) & 0xffffffff) | polyProd[3] & 0xffffffff;
+	}
+}
+
+/*
+ * Similar to the regular KeyExpansion routine but every word in the expandedKey array except the first
+ * and last N_b words is transformed by a modified InvMixColumns() transformation.
+ */
+
+void EqKeyExpansion(const uint8_t key[], uint32_t expandedKey[]) {
+	int i = 0;
+	while (i < N_k) {
+		// Converting 4 bytes from key into a word one at a time to form the initial round keys, works on rows not columns
+		expandedKey[i] = (key[4 * i] << 24) | (key[4 * i + 1] << 16) | (key[4 * i + 2] << 8) | key[4 * i + 3];
+		//expandedKey[i] = *(uint32_t *)&key[4 * i];
+		i += 1;
+	} // i = N_k
+	while (i < N_b * (N_r + 1)) {
+		uint32_t temp = expandedKey[i - 1];
+		if (i % N_k == 0) {
+			temp = SubWords(RotWord(temp)) ^ Rcon[i / N_k - 1];
+		} else if ((N_k > 6) && (i % N_k == 4)) {
+			temp = SubWords(temp);
+		}
+		expandedKey[i] = expandedKey[i - N_k] ^ temp;
+		i += 1;
+	}
+	uint32_t dw[4];
+	// The transformation is applied here -->
+	for (i = 1; i < N_r; i++) {
+		for (int j = i * N_b; j < (i + 1) * N_b; j++) {
+			dw[j - i * N_b] = expandedKey[j];
+		}
+		EqInvMixColumns(dw);
+		for (int j = i * N_b; j < (i + 1) * N_b; j++) {
+			expandedKey[j] = dw[j - i * N_b];
+		}
+	}
+}
+
+/*
+ * The Inverse Cipher to retrieve the stateArray after EncryptCipher()
+ */
+
+void InverseCipher(uint8_t stateArray[], const uint32_t roundKeys[]) {
+	uint32_t w[N_b];
+
+	for (int i = N_r * N_b; i < (N_r + 1) * N_b; i++) {
+		w[i - N_r * N_b] = roundKeys[i];
+	}
+	AddRoundKey(stateArray, w);
+
+	for (int i = N_r - 1; i > 0; i--) {
+
+		InvShiftRows(stateArray);
+
+		InvSubBytes(stateArray);
+
+		for (int j = i * N_b; j < (i + 1) * N_b; j++) {
+			w[j - i * N_b] = roundKeys[j];
+		}
+		AddRoundKey(stateArray, w);
+
+		InvMixColumns(stateArray);
+	}
+	InvShiftRows(stateArray);
+	InvSubBytes(stateArray);
+
+	for (int i = 0; i < N_b; i++) {
+		w[i] = roundKeys[i];
+	}
+	AddRoundKey(stateArray, w);
+}
+
+/*
+ * Equivalent Inverse Cipher: To be used with EqKeyExpansion(), which applies InvMixColumns() to the roundKeys.
+ */
+
+void EqInverseCipher(uint8_t stateArray[], const uint32_t droundKeys[]) {
+	uint32_t dw[N_b];
+
+	for (int i = N_r * N_b; i < (N_r + 1) * N_b; i++) {
+		dw[i - N_r * N_b] = droundKeys[i];
+	}
+	AddRoundKey(stateArray, dw);
+
+	for (int i = N_r - 1; i > 0; i++) {
+		InvSubBytes(stateArray);
+		InvSubBytes(stateArray);
+		InvMixColumns(stateArray);
+		for (int j = i * N_b; j < (i + 1) * N_b; j++) {
+			dw[j - i * N_b] = droundKeys[j];
+		}
+		AddRoundKey(stateArray, dw);
+	}
+	InvSubBytes(stateArray);
+	InvShiftRows(stateArray);
+	for (int i = 0; i < N_b; i++) {
+		dw[i] = droundKeys[i];
+	}
+	AddRoundKey(stateArray, dw);
 }
 
 int main(int argc, char **argv) {
